1bd2fd0aaab03a90da46953a8a91b8ffaa544483
HEBRAS <majed.ttrk@gmail.com>
2025-12-08 01:09:31 +0300

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -110,3 +110,22 @@ document.addEventListener("click", (e) => {
 
     }, 1500);
 });
+
+
+// انيميشن انفجار القلب عند الضغط
+document.addEventListener("click", (e) => {
+    const container = document.getElementById("heart-container");
+    const heart = document.createElement("div");
+    heart.classList.add("heart");
+
+    // ضبط مكان ظهور القلب
+    heart.style.left = e.clientX - 10 + "px";
+    heart.style.top = e.clientY - 10 + "px";
+
+    container.appendChild(heart);
+
+    // إزالة القلب بعد الانيميشن
+    setTimeout(() => {
+        heart.remove();
+    }, 1600);
+});
